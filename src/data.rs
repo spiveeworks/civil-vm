41551fecc064cfda50b55d7721f8f8ea985b1108
SpiveeWorks <spiveehere@gmail.com>
2018-05-07 15:27:36 +1000

    use totemcell and &'static

--- src/data.rs
+++ src/data.rs
@@ -1,17 +1,22 @@
+use prelude::*;
+use programs;
+
 pub enum Field {
     Integer(i64),  // for numbers with conservation, and C-enums, and indeces
     Real(f64),  // for any other kind of number
     Data(Data),  // substructures
     Ref(Ref),
+    // Weak(WeakRef),
+    // List(???),
 }
 
 pub struct Data {
-    var: usize,  // variant of the enum
+    var: usize,  // variant
     fields: Vec<Field>,  // could also be a hashmap: String -> Field
 }
 
 pub struct Ref {
-    table: ::programs::Table,
-    data: ::std::rc::Rc<Data>, // totem cell?
+    table: &'static programs::Table,
+    data: Strong<Data>,
 }
 
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,24 @@
+extern crate totem_cell;
 
 pub mod data;
 pub mod programs;
+
+pub mod prelude {
+    pub use totem_cell::Totem;
+    pub type Cell<T> = ::totem_cell::TotemCell<T>;
+
+    pub type Strong<T> = ::std::rc::Rc<Cell<T>>;
+    pub type Weak<T> = ::std::rc::Weak<Cell<T>>;
+
+
+    pub fn strong<T>(val: T) -> Strong<T> where T: 'static {
+        ::std::rc::Rc::new(Cell::new(val))
+    }
+
+
+    pub fn heap<T: 'static>(val: T) -> &'static T {
+        let as_box = Box::new(val);
+        let as_ptr = Box::into_raw(as_box);
+        unsafe { &*as_ptr }
+    }
+}
