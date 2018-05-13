7a80ea942e2702e03432142d8cb1eeecc4879340
SpiveeWorks <spiveehere@gmail.com>
2018-05-13 15:05:37 +1000

    virtual method calls or whatever im going to call them

--- src/data.rs
+++ src/data.rs
@@ -10,12 +10,38 @@ pub enum Field {
     // List(???),
 }
 
+impl Field {
+    pub fn num(self: &Self) -> f64 {
+        match *self {
+            Field::Num(result) => result,
+            _ => panic!("Expected number"),
+        }
+    }
+
+    pub fn data(self: &Self) -> &Data {
+        match *self {
+            Field::Data(ref result) => result,
+            _ => panic!("Expected data"),
+        }
+    }
+
+    pub fn entity(self: &Self) -> &EntityRef {
+        match *self {
+            Field::Entity(ref result) => result,
+            _ => panic!("Expected entity"),
+        }
+    }
+}
+
 pub struct Data {
     variant: usize,
     fields: Dict<Field>,
 }
 
+pub type EntityType = Dict<programs::Table>;
+
 pub struct EntityData {
+    pub type_name: String,
     event: Option<()>,
     action: String,
     state: String,
@@ -23,7 +49,7 @@ pub struct EntityData {
 }
 
 pub struct EntityRef {
-    table: String,
-    data: Strong<EntityData>,
+    pub table: String,
+    pub data: Strong<EntityData>,
 }
 
--- src/programs.rs
+++ src/programs.rs
@@ -7,6 +7,15 @@ pub enum TableTerm {
     Action(Action),
 }
 
+impl TableTerm {
+    pub fn action(self: &Self) -> &Action {
+        match *self {
+            TableTerm::Action(ref result) => result,
+            // _ => panic!("Expected action"),
+        }
+    }
+}
+
 pub struct Table {
     terms: Dict<TableTerm>,
 }
@@ -15,30 +24,76 @@ type Action = Vec<Statement>;
 
 pub enum Statement {
     Debug(String),
-    ExecTable(usize),
+    ExecEntity {
+        ent_name: String,
+        action_name: String,
+        args: Vec<String>,
+    },
 }
 
+fn get_action<'a>(
+    types: &'a Dict<data::EntityType>,
+
+    entity_type_name: &String,
+    table_name: &String,
+    action_name: &String,
+) -> &'a Action {
+    let entity_type = &types[entity_type_name];
+    let table = &entity_type[table_name];
+    table.terms[action_name].action()
+}
 
 pub fn execute(
+    totem: &mut Totem,
     game: &mut game::Game,
-    mut code: &Action,
-    mut pc: usize,
-    mut vars: Vec<data::Field>
+    types: &Dict<data::EntityType>,
+
+    entity: Strong<data::EntityData>,
+    table_name: String,
+    action_name: String,
+
+    mut vars: Dict<data::Field>
 ) {
+    // current continuation
+    let mut cc = Some((entity, table_name, action_name));
+
     loop {
-        match code[pc] {
-            Statement::Debug(ref to_print) => {
-                println!("Debug: {}", to_print);
-            },
-            Statement::ExecTable(table_term) => {
-                /*
-                let table = vars.pop().unwrap().table();
-                let term = table[table_term];
-                let (new_code, new_pc) = term.action();
-                code = new_code;
-                pc = new_pc;
-                */
-            },
+        let (entity, table, action) =
+            cc.take().unwrap();
+
+        let entity_type= {
+            let entity = entity.borrow(totem);
+            entity.type_name.clone()
+        };
+
+        let code = get_action(types, &entity_type, &table, &action);
+        let mut pc = 0;
+
+        while cc.is_none() {
+            match code[pc] {
+                Statement::Debug(ref to_print) => {
+                    println!("Debug: {}", to_print);
+                },
+                Statement::ExecEntity {
+                    ref ent_name,
+                    ref action_name,
+                    ref args,
+                } => {
+                    {
+                        let entity_ref = vars[ent_name].entity();
+
+                        let entity = Strong::clone(&entity_ref.data);
+                        let table_name = entity_ref.table.clone();
+                        let action_name = action_name.clone();
+
+                        cc = Some((entity, table_name, action_name));
+                    }
+
+                    vars.retain(|k, _| args.contains(k));
+                },
+            }
+
+            pc += 1;
         }
     }
 }
