5fdce2b31f3694635fff9dcbb8e616772db64f08
SpiveeWorks <spiveehere@gmail.com>
2018-06-04 20:25:53 +1000

    separate wait into new command, restructure loops

--- src/algorithm.rs
+++ src/algorithm.rs
@@ -26,8 +26,8 @@ pub enum Statement {
     State {
         name: String,
         terms: Dict<String>,
-        wait: Option<Time>,
     },
+    Wait(Time),
     CancelWait,
     SetAdd {
         set_name: String,
@@ -65,7 +65,7 @@ pub fn execute_init(
     table_name: String,
     init_name: String,
     args: Vec<data::Field>,
-) -> data::Entity {
+) -> data::EntityRef {
     let args = item::get_algorithm(
         types,
         &type_name,
@@ -76,75 +76,25 @@ pub fn execute_init(
         .cloned()
         .zip(args)
         .collect();
-    let result = data::EntityData::new(type_name);
-    execute_reaction(
+
+    let table = table_name.clone();
+    let data = data::EntityData::new(type_name);
+
+    execute_action(
         totem,
         event_queue,
         types,
-        Strong::clone(&result),
 
+        Strong::clone(&data),
         table_name,
         init_name,
-        args
-    );
-    result
-}
 
-pub fn continue_action(
-    totem: &mut Totem,
-    event_queue: &mut event::EventQueue,
-    types: &Dict<item::EntityType>,
-
-    entity: Strong<data::EntityData>,
-    table_name: String,
-    action_name: String,
-    pc: usize,
-) {
-    let vars = {
-        let entity = entity.borrow_mut(totem);
-        entity.event = None;
-
-        mem::replace(&mut entity.data, Dict::new())
-    };
-    execute_action(
-        totem,
-        event_queue,
-        types,
-
-        entity,
-        table_name,
-        action_name,
-
-        vars,
-        pc,
+        Some(args),
+        0,
         false,
     );
-}
 
-fn execute_reaction(
-    totem: &mut Totem,
-    event_queue: &mut event::EventQueue,
-    types: &Dict<item::EntityType>,
-
-    entity: Strong<data::EntityData>,
-    table_name: String,
-    action_name: String,
-
-    vars: Dict<data::Field>,
-) {
-    execute_action(
-        totem,
-        event_queue,
-        types,
-
-        entity,
-        table_name,
-        action_name,
-
-        vars,
-        0,
-        true,
-    );
+    data::EntityRef { table, data }
 }
 
 pub fn execute_action(
@@ -156,12 +106,86 @@ pub fn execute_action(
     table_name: String,
     action_name: String,
 
+    vars: Option<data::Data>,  // None to use entity's state
+    pc: usize,
+    mut has_state: bool,
+) {
+    let vars = {
+        if let Some(vars) = vars {
+            vars
+        } else {
+            has_state = false;
+
+            let entity = entity.borrow_mut(totem);
+            entity.event = None;
+
+            mem::replace(&mut entity.data, Dict::new())
+        }
+    };
+
+    let result = execute_algorithm(
+        totem,
+        event_queue,
+        types,
+
+        entity,
+        table_name,
+        action_name,
+
+        vars,
+        pc,
+        has_state,
+    );
+
+    if let AlgorithmResult::ExternContinuation {
+        entity,
+        table_name,
+        action_name,
+        vars,
+    } = result {
+        execute_action(
+            totem,
+            event_queue,
+            types,
+
+            entity,
+            table_name,
+            action_name,
+
+            Some(vars),
+            0,
+            true,
+        );
+    }
+}
+
+pub enum AlgorithmResult {
+    ExternContinuation {
+        entity: data::Entity,
+        table_name: String,
+        action_name: String,
+        vars: Dict<data::Field>,
+    },
+    ContinueLoop {
+        vars: Dict<data::Field>,
+    },
+    ReturnVals(Vec<data::Field>),
+}
+
+pub fn execute_algorithm(
+    totem: &mut Totem,
+    event_queue: &mut event::EventQueue,
+    types: &Dict<item::EntityType>,
+
+    entity: Strong<data::EntityData>,
+    table_name: String,
+    action_name: String,
+
     mut vars: Dict<data::Field>,
     mut pc: usize,
     mut has_state: bool,
-) {
-    // current continuation
-    let mut cc = None;
+) -> AlgorithmResult {
+    let mut result = None;
 
     let type_name = {
         let entity = entity.borrow(totem);
@@ -175,27 +199,42 @@ pub fn execute_action(
         &action_name
     ).steps;
 
-    while pc < code.len() && cc.is_none() {
+    while pc < code.len() && result.is_none() {
         match code[pc] {
             Statement::Debug(ref to_print) => {
                 println!("Debug: {}", to_print);
             },
             Statement::ExecEntity {
                 ref ent_name,
-                ref action_name,
+                action_name: ref new_action_name,
                 ref args,
             } => {
-                {
-                    let entity_ref = vars[ent_name].entity();
+                pc += 1;
+                if let Some(&Statement::Wait(time)) = code.get(pc) {
+                    wait(
+                        totem,
+                        event_queue,
 
-                    let entity = Strong::clone(&entity_ref.data);
-                    let table_name = entity_ref.table.clone();
-                    let action_name = action_name.clone();
+                        &entity,
 
-                    cc = Some((entity, table_name, action_name));
+                        table_name.clone(),
+                        action_name.clone(),
+                        pc,
+
+                        time,
+                    );
                 }
 
-                vars = extract(&mut vars, args);
+                let new_entity = vars[ent_name].entity().clone();
+
+                let new_vars = extract(&mut vars, args);
+
+                result = Some(AlgorithmResult::ExternContinuation {
+                    entity: new_entity.data,
+                    table_name: new_entity.table,
+                    action_name: new_action_name.clone(),
+                    vars: new_vars,
+                });
             },
             Statement::Assign {
                 ref results,
@@ -216,31 +255,34 @@ pub fn execute_action(
             Statement::State {
                 ref name,
                 ref terms,
-                wait,
             } => {
                 if has_state {
                     panic!("Tried to overwrite state without cancelling");
                 }
 
-                let state_name = name.clone();
+                let entity = entity.borrow_mut(totem);
 
-                update_state(
+                entity.state_name = name.clone();
+                entity.data = extract(&mut vars, terms);
+
+                has_state = true;
+            }
+
+            Statement::Wait(time) => {
+                wait(
                     totem,
                     event_queue,
 
                     &entity,
-                    &mut vars,
-                    terms,
 
                     table_name,
                     action_name,
-                    state_name,
-
                     pc,
-                    wait,
+
+                    time,
                 );
 
-                return;
+                break;
             },
             Statement::CancelWait => {
                 let entity = entity.borrow_mut(totem);
@@ -302,80 +344,36 @@ pub fn execute_action(
             println!("Warning: Code after external call will not be executed
 without creating a new entity state");
         }
-    } else if let Some(&Statement::State {
-        ref name,
-        ref terms,
-        wait,
-    }) = code.get(pc) {
-        let state_name = name.clone();
-
-        update_state(
-            totem,
-            event_queue,
-
-            &entity,
-            &mut vars,
-            terms,
-
-            table_name,
-            action_name,
-            state_name,
-
-            pc,
-            wait,
-        );
     } else {
         panic!("Tried to exit without resetting state");
     }
 
-    if let Some((entity, table_name, action_name)) = cc {
-        execute_reaction(
-            totem,
-            event_queue,
-            types,
-            entity,
-            table_name,
-            action_name,
-            vars
-        );
-    }
+    result.unwrap_or_else(|| AlgorithmResult::ReturnVals(Vec::new()))
 }
 
-fn update_state(
+fn wait(
     totem: &mut Totem,
     event_queue: &mut event::EventQueue,
 
-    entity: &data::Entity,
-    vars: &mut data::Data,
-    terms: &Dict<String>,
+    entity_: &data::Entity,
 
     table_name: String,
     action_name: String,
-    state_name: String,
+    mut pc: usize,
 
-    pc: usize,
-    wait: Option<Time>,
+    time: Time,
 ) {
-    let data = extract(vars, terms);
-    let event = {
-        if let Some(time) = wait {
-            let entity = Strong::clone(entity);
-            let event = event::Event { entity, table_name, action_name, pc };
+    let entity = Strong::clone(entity_);
+    pc += 1;
 
-            let absolute_time = event_queue.now() + time;
-            let id = event_queue.enqueue_absolute(event, absolute_time);
+    let event = event::Event { entity, table_name, action_name, pc };
 
-            Some(event::EventHandle(absolute_time, id))
-        } else {
-            None
-        }
-    };
+    let absolute_time = event_queue.now() + time;
+    let id = event_queue.enqueue_absolute(event, absolute_time);
 
-    let entity = entity.borrow_mut(totem);
+    let entity = entity_.borrow_mut(totem);
 
-    entity.state_name = state_name;
-    entity.data = data;
-    entity.event = event;
+    entity.event = Some(event::EventHandle(absolute_time, id));
 }
 
 fn evaluate_expression(
@@ -458,7 +456,7 @@ fn evaluate_expression_into(
                 args,
                 vars,
             );
-            let result_val = execute_init(
+            let result_ref = execute_init(
                 totem,
                 event_queue,
                 types,
@@ -469,8 +467,6 @@ fn evaluate_expression_into(
                 args
             );
 
-            let table = table_name.clone();
-            let result_ref = data::EntityRef { data: result_val, table };
             let result_term = data::Field::Entity(result_ref);
             result.push(result_term);
         },
