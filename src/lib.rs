a439e94f564173e2481a91babf8bce7377dd05a8
SpiveeWorks <spiveehere@gmail.com>
2018-06-15 18:09:37 +1000

    note about annoying error

--- src/lib.rs
+++ src/lib.rs
@@ -12,6 +12,7 @@ pub mod table;
 pub mod prelude {
     pub use instance::FlopInstance;
     pub use instance::Flop;
+    // Gives an unused import error, but can't be removed.
     pub(crate) use instance::FlopParts;
 
     // Units (just Time)
